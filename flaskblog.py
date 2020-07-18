58bde2ff631759ff874a2d81b931f3fc8ffd38b1
Ashley Mosoti <ninahb746@gmail.com>
2020-07-19 00:30:02 +0300

    Use bootstrap for styling the page

--- flaskblog.py
+++ flaskblog.py
@@ -5,13 +5,13 @@ posts = [
     {
         'author': 'Ninah Mozzy',
         'title': 'Blog post 1',
-        'content': 'First post content',
+        'content': 'Hello Loves',
         'date_posted': 'April 1, 2020'
     },
     {
         'author': 'simpo boy',
         'title': 'Blog post 2',
-        'content': 'Second post content',
+        'content': 'Ndio manake',
         'date_posted': 'April 4, 2020'
     }
 ]
@@ -23,7 +23,7 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
