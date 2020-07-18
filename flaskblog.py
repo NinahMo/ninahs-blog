9bee8be030e84d02d2d0623ff52c895ad90dc490
Ashley Mosoti <ninahb746@gmail.com>
2020-07-18 23:04:14 +0300

    Create a for loop for the posts

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,29 @@
-from flask import Flask
+from flask import Flask,render_template 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Ninah Mozzy',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'April 1, 2020'
+    },
+    {
+        'author': 'simpo boy',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 4, 2020'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
