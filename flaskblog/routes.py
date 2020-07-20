d4aa1779d7a0bf35989c0a1c9cc2a1f0095ae550
Ashley Mosoti <ninahb746@gmail.com>
2020-07-21 02:58:27 +0300

    Add a form for the account on its html file

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template,url_for,flash,redirect,request
 from flaskblog import app,db,bcrypt
-from flaskblog.forms import RegistrationForm,LoginForm 
+from flaskblog.forms import RegistrationForm,LoginForm, UpdateAccountForm
 from flaskblog.models import User,Post
 from flask_login import login_user,current_user,logout_user,login_required
 
@@ -67,5 +67,7 @@ def logout():
 @app.route("/account")
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    image_file = url_for('static', filename='images/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
 
